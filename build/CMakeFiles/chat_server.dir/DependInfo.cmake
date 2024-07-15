65bf808391c335fbd8d168cc54882cc17ac38d34
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-07-16 03:05:51 +0530

    cmake

---
+++ build/CMakeFiles/chat_server.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/vikra/OneDrive/Desktop/Chat-Application/chat_server.cpp" "CMakeFiles/chat_server.dir/chat_server.cpp.o" "gcc" "CMakeFiles/chat_server.dir/chat_server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
